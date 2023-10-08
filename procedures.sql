a63a86eca584b63cddb1651261b139c6214f830b
André Luiz de Oliveira Vasconcelos <alovasconcelos@gmail.com>
2023-10-08 10:53:58 -0300

    ...

--- procedures.sql
+++ procedures.sql
@@ -1,5 +1,5 @@
-﻿ delimiter //
-
+﻿drop procedure CancelarVendaPDV;
+ delimiter //
 CREATE PROCEDURE CancelarVendaPDV (IN argVendaCanceladaId INT, 
         IN  argUsuarioId INT)
   BEGIN
@@ -28,6 +28,7 @@ CREATE PROCEDURE CancelarVendaPDV (IN argVendaCanceladaId INT,
 
 delimiter ;
 
+drop procedure CancelarDetalheVendaPDV;
  delimiter //
 
 CREATE PROCEDURE CancelarDetalheVendaPDV (
@@ -52,56 +53,3 @@ delimiter ;
 
 DELIMITER //
 
-CREATE VIEW vMovimentoCaixa AS
-SELECT
-    m.DataHora,
-    m.UsuarioId,
-    m.Tipo,
-    m.SaldoAnterior,
-    m.Valor,
-    m.Saldo,
-    CASE 
-		WHEN VendaPagamentoId IS NULL THEN FALSE
-        ELSE TRUE
-    END AS Venda,
-    vp.FormaPagamentoId,
-    CASE
-		WHEN fp.Descricao IS NOT NULL THEN CONCAT('Venda em ', fp.Descricao)
-        ELSE 
-        CASE
-			WHEN tipo = 'E' THEN 'Entrada'
-            ELSE 'Sangria'
-        END 
-	END AS Descricao
-FROM
-	MovimentoCaixa m
-    LEFT JOIN VendaPagamento vp ON m.VendaPagamentoId = vp.Id
-    LEFT JOIN FormaPagamento fp ON vp.FormaPagamentoId = fp.Id
-ORDER BY
-	m.DataHora;
-
-CREATE PROCEDURE Caixa (IN usuarioId INT, IN tipo VARCHAR(1), IN valor DECIMAL(12,2), IN vendaPagamentoId INT)
-  BEGIN
-
-    DECLARE saldoAnterior DECIMAL(12, 2);
-    DECLARE saldoAtual DECIMAL(12, 2);    
-
-    SELECT COALESCE(MAX(saldo),0) INTO saldoAnterior FROM MovimentoCaixa WHERE UsuarioId = usuarioId; 
-
-    IF tipo = 'E' THEN
-	    -- Entrada
-	    SET saldoAtual = saldoAnterior + valor;
-    ELSE
-	    -- Saída
-	    SET saldoAtual = saldoAnterior - valor;
-    END IF;
-
-    INSERT INTO MovimentoCaixa 
-	    (UsuarioId, Tipo, SaldoAnterior, Valor, Saldo, VendaPagamentoId) 
-	    VALUES
-	    (usuarioId, tipo, saldoAnterior, valor, saldoAtual, vendaPagamentoId);
-
-    
-  END//
-
-DELIMITER ;
